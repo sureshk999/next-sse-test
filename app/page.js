d49d1c98bd43a6be953826689ce5fb8a9e00d32e
SURESH KHIRWADKAR <suresh.khirwadkar@gmail.com>
2023-12-25 12:13:16 +1000

    change port

---
+++ app/page.js
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useEffect, useState } from 'react';
+
+export default function Test() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const eventSource = new EventSource('http://localhost:3005/events');
+    eventSource.onmessage = (event) => {
+      const message = event.data;
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+    eventSource.onerror = () => {
+      eventSource.close();
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>Test SSE</h1>
+      {messages.map((msg, index) => (
+        <p key={index}>{msg}</p>
+      ))}
+    </div>
+  );
+}
